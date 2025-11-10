5f3b22af9ad940c74d7f9063216ef16190f10259
christiangrumbling4747-ux <christiangrumbling4747@gmail.com>
2025-11-10 12:11:42 -0500

    Add files via upload
    added the singup/login page with manger username and password.

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('customer-form').addEventListener('submit', function(event){
+    event.preventDefault();
+const customerName = document.getElementById('customer-name').value;
+localStorage.setItem('customerName', customerName);
+window.location.href = 'menu.html';
+});
